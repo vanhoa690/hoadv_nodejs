8d2c7f63f7a198bcadd3a42650303f2d658a7a5f
HoadvRP <hoadv@reactplus.jp>
2023-11-18 13:11:53 +0700

    update mongodb alat

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ app.use(express.urlencoded({
 app.use(express.json());
 
 // Template engine
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
@@ -24,33 +22,6 @@ const db = require('./config/db');
 db.connect();
 
 routes(app)
-// app.get("/", (req, res) => {
-//   // return res.send('Hello World!');
-//   res.render("home");
-// });
-
-// app.get("/products/:id", (req, res) => {
-//   const {query, params, body} = req
-//   console.log(query)
-//   console.log(params)
-//   console.log(body)
-//   // return res.send('Hello World!');
-//   res.render("home");
-// });
-
-// app.post("/products", (req, res) => {
-//   const { body} = req
-//   console.log(body)
-//   // return res.send('Hello World!');
-//   res.render("home");
-// });
-
-// app.get("/search", (req, res) => {
-// //   return res.send("Hello search!");
-// const {query, params, body} = req
-//   console.log(query)
-// res.render("search");
-// });
 
 app.listen(port, () =>
   console.log(`App listening at http://localhost:${port}`)
