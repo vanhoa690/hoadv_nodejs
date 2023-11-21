9d6d74560b211f9b025b904ec1d288b090ba8ee4
hoadv <vanhoa690@gmail.com>
2023-11-21 08:46:13 +0700

    add user ok

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,17 +1,32 @@
 const User = require('../models/UserModel');
+const bcryptjs = require('bcryptjs');
 
 class UsersController {
   // [GET] /users
   async getAllUsers(req, res) {
     try {
-      const products = await User.find();
-      res.json(products);
+      const users = await User.find();
+      res.json(users);
     } catch (error) {
       res.status(400).json({ error: 'ERROR!!!' });
     }
   }
 
- 
+  // [POST] /users
+  async createUser(req, res) {
+    try {
+      const { username, email, password } = req.body;
+      const hashPassword = await bcryptjs.hash(password, 10);
+      await User.create({
+        username,
+        email,
+        password: hashPassword,
+      });
+      res.status(200).json({ message: 'Add user successfull' });
+    } catch (error) {
+      res.status(400).json({ error: 'ERROR!!!' });
+    }
+  }
 }
 
 module.exports = new UsersController();
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const usersController = require('../controllers/UsersController')
 
 router.get('/', usersController.getAllUsers)
+router.post('/', usersController.createUser)
 
 module.exports = router;
